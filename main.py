489f741efb7f7bf2cb1c5b0928f8b83b29bd8e15
Nayal17 <himanshunayalop@gmail.com>
2022-11-23 13:44:34 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from preprocess import preprocess
+from recommendation import recommend
 
 def read_file(path=None):
     df = pd.read_csv(path)
@@ -9,3 +10,4 @@ if __name__=='__main__':
     path = r'Dataset\tmdb_movies.csv'
     df = read_file(path)
     df = preprocess(df)
+    recommended_movies = recommend(df,'Avatar')
