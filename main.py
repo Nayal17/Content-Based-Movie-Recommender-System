a4c92a821318c3d0e0cdbecb9afc1cf32847694b
Nayal17 <himanshunayalop@gmail.com>
2022-11-23 11:13:42 +0530

    added read_file function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from preprocess import preprocess
 
+def read_file(path=None):
+    df = pd.read_csv(path)
+    return df
+
 if __name__=='__main__':
-    df = pd.read_csv(r'Dataset\tmdb_movies.csv')
+    path = r'Dataset\tmdb_movies.csv'
+    df = read_file(path)
     df = preprocess(df)
