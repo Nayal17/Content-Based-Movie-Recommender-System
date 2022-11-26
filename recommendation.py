7a3331187850b025c32b5e42056f1260006f43d2
Nayal17 <nayal17.916@gmail.com>
2022-11-26 21:53:21 +0530

    fetched poster and ratings

--- recommendation.py
+++ recommendation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import requests
 
 def vectors(df):
     tfidf = TfidfVectorizer()
@@ -12,15 +13,28 @@ def similarity(df):
     similarity_ = cosine_similarity(vectors_)
     return similarity_
 
+def fetch_poster(movie_id):
+    response = requests.get(
+        "https://api.themoviedb.org/3/movie/{}?api_key=76df17040ad5eedc415f09ac17c35ed5".format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data["poster_path"], data['vote_average']
+
 def recommend(df,movie=None):
     movie_index = df[df['title']==movie].index[0]
     similarity_all = similarity(df)
     similarity_ = similarity_all[movie_index]
-    top_10_recommended_movies = sorted(enumerate(similarity_),reverse=True,key=lambda x:x[1])[1:11]
+    top_8_recommended_movies = sorted(enumerate(similarity_),reverse=True,key=lambda x:x[1])[1:9]
     rec_movies = []
-    for movie in top_10_recommended_movies:
-        movie = movie[0]
-        rec_movies.append(df.iloc[movie].title)
+    movie_posters = []
+    user_scores = []
+    for movie in top_8_recommended_movies:
+        idx = movie[0]
+        rec_movies.append(df.iloc[idx].title)
+        movie_posters.append(fetch_poster(df.iloc[idx].id)[0])
+        user_scores.append(fetch_poster(df.iloc[idx].id)[1])
 
-    return rec_movies
+    return rec_movies, movie_posters, user_scores
+
+
+    
 
